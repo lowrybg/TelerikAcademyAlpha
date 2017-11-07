3aa355e1177726a82a5be1eedd29cd0f00ca154f
lowrybg <lowrybg@gmail.com>
2017-11-07 19:48:08 +0200

    Arrays

---
+++ 02.Arrays/MaxmimalSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//unsolved
+namespace MaxmimalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            int sum = 0;
+            int final = 0;
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += numbers[i];
+                if(sum>final)
+                {
+                    final += sum;
+                }
+
+                
+            }
+
+            Console.WriteLine(final);
+        }
+    }
+}
